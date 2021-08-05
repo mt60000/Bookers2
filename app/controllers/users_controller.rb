60c0d66a6288b900ab051760d9218864c440777d
mt60000 <niruyaku45@gmail.com>
2021-08-05 08:17:12 +0000

    First Commit

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,12 @@
+class UsesController < ApplicationController
+ 
+
+  def index
+  end
+
+  def show
+  end
+
+  def edit
+  end
+end
---
+++ test/controllers/uses_controller_test.rb
@@ -0,0 +1,24 @@
+require 'test_helper'
+
+class UsesControllerTest < ActionDispatch::IntegrationTest
+  test "should get controller" do
+    get uses_controller_url
+    assert_response :success
+  end
+
+  test "should get index" do
+    get uses_index_url
+    assert_response :success
+  end
+
+  test "should get show" do
+    get uses_show_url
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get uses_edit_url
+    assert_response :success
+  end
+
+end

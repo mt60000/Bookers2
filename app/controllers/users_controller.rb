93dfb34585750991988badf6d4d4263866b3bccf
mt60000 <niruyaku45@gmail.com>
2021-08-06 13:39:46 +0000

    header,footer完成 sign_in sign_up途中

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,13 @@
-class UsesController < ApplicationController
- 
+class UsersController < ApplicationController
+
 
   def index
+    @users = User.all
   end
 
   def show
+    @user = User.find(params[:id])
+    
   end
 
   def edit
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
+  get 'about' => 'homes#about'
+  resources :users, only: [:index, :show, :edit, :update]
+  resources :books, only: [:new, :create, :index, :show, :edit, :update, :destroy]
 end

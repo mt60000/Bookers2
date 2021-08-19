3f18a5b9967cbd779a14ad667a01c401c5e3d066
mt60000 <niruyaku45@gmail.com>
2021-08-19 13:56:30 +0000

    いいね、コメント機能のモデルと関連付け終了.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
+  get 'favorites/create'
+  get 'favorites/destroy'
+  get 'book_comments/create'
+  get 'book_comments/destroy'
   devise_for :users
   root to: 'homes#top'
   get 'home/about' => 'homes#about'
   resources :users, only: [:index, :show, :edit, :update]
-  resources :books, only: [:new, :create, :index, :show, :edit, :update, :destroy]
+  resources :books, only: [:new, :create, :index, :show, :edit, :update, :destroy] do
+    resources :favorites, only: [:create, :destroy]
+    resources :book_comments, only: [:create, :destroy]
+  end
 end

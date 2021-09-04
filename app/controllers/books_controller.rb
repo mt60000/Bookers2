cab11625493ba6be1a6347045c55e160dbeac1da
mt60000 <niruyaku45@gmail.com>
2021-09-04 13:29:21 +0000

    課題7途中

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,7 +2,21 @@ class BooksController < ApplicationController
   before_action :authenticate_user!, excect: [:index, :show]
 
   def index
+    to = Time.current.at_end_of_day
+    from = (to - 6.day).at_beginning_of_day
     @books = Book.all
+    @books_rank = Book.includes(:favorited_users)
+                      .sort {|a,b|
+                        b.favorited_users.includes(:favorites).where(created_at: from...to).count <=>
+                        a.favorited_users.includes(:favorites).where(created_at: from...to).count
+                      }
+
+
+    # @books_rank = Book.all
+    #                   .(Favorite.group(:book_id)
+    #                   .where(created_at: from...to)
+    #                   .order('count(book_id) desc')
+    #                   .pluck(:book_id))
     @book =Book.new
     @user = current_user
   end

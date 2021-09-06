b9f8005785a69848d94ce1bf5365cfd606f9b73d
mt60000 <niruyaku45@gmail.com>
2021-09-06 01:48:06 +0000

    課題8

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -5,10 +5,11 @@ class RoomsController < ApplicationController
     @room = Room.create
     @entry1 = Entry.create(room_id: @room.id, user_id: current_user.id)
     @entry2 = Entry.create(params.require(:entry).permit(:user_id, :room_id).merge(room_id: @room.id))
-    redirect_to "/room/#{@room.id}"
+    redirect_to "/rooms/#{@room.id}"
   end
 
   def show
+    @user = User.find(params[:id])
     @room = Room.find(params[:id])
     if Entry.where(user_id: current_user.id, room_id: @room.id).present?
       @messages = @room.messages
